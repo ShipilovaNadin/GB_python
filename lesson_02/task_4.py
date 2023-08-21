bc15dd719e49412c4827cc3ea37d805246d95cb3
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2023-08-21 21:11:06 +0300

    add 2 seminar_02.

--- lesson_02/task_4.py
+++ lesson_02/task_4.py
@@ -6,6 +6,8 @@
 ✔ Точность вычислений должна составлять
 не менее 42 знаков после запятой.
 """
+import decimal
+import math
 
 r = int(input('Введите радиус: '))
 
@@ -14,3 +16,17 @@ i = 2 * r * 3.14  # Длина окружности
 s = 3.14 * r ** 2  # Площадь круга
 
 print(f"Длина окружности: {i:.42f}, площадь круга: {s:.42f}")
+
+""" Вариант 2"""
+LEN_DIG = 42
+MAX_D = 1000
+PI = decimal.Decimal(math.pi)
+
+decimal.getcontext().prec = LEN_DIG
+diameter = decimal.Decimal(input("Введите диаметр окружности: "))
+if diameter > MAX_D:
+    print("Превысили допустимое значение диаметра")
+else:
+    len_circles = PI * diameter
+    area_circles = PI * diameter ** 2 / 4
+    print(f"{len_circles=}\n{area_circles=}")
