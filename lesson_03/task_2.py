c27b2a6b78f221c6e0d8201e1e4e211a5a6309a9
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2023-08-26 09:58:03 +0300

    start 4 seminar

--- lesson_03/task_2.py
+++ lesson_03/task_2.py
@@ -9,22 +9,38 @@
 хотя бы одна заглавная буква
 ✔ Строку в нижнем регистре в остальных случаях
 """
+#
+# lst = [1, 2.1, True, -5, "Sds"]
+# for el in lst:
+#     try:
+#         print(int(el))
+#         print(float(el))
+#     except ValueError:
+#         print(f"Невозможно преобразовать {el} к целому положительному числу")
+#         print(f"Невозможно преобразовать {el} к вещественному числу")
+#
+# for el in lst:
+#     try:
+#         if type(el) == str and not el.islower():
+#             print(el.lower())
+#     except TypeError:
+#         print("Ошибка")
+#
 
-lst = [1, 2.1, True, -5, "Sds"]
-for el in lst:
-    try:
-        print(int(el))
-        print(float(el))
-    except ValueError:
-        print(f"Невозможно преобразовать {el} к целому положительному числу")
-        print(f"Невозможно преобразовать {el} к вещественному числу")
+# вариант 2
 
-for el in lst:
-    try:
-        if type(el) == str and not el.islower():
-            print(el.lower())
-    except TypeError:
-        print("Ошибка")
+a = input("Введите данные: ")
 
+if a.isdigit():
+    a = int(a)
+elif a.replace(".", "").replace("-", "").isdigit()\
+    and a.count(".") == 1\
+    and a.count("-") <= 1\
+    and (a.count("-") == 1 and a.startswith("-")):
+    a = float(a)
+elif a != a.lower():
+    a = a.lower()
+else:
+    a = a.upper()
 
-
+print(type(a), a)
