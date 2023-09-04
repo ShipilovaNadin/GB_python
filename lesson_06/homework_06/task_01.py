dc2bc8942d4ac2ad288e090c77c1fbc4c0a710e3
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2023-09-04 15:09:48 +0300

    add 6 homework

---
+++ lesson_06/homework_06/task_01.py
@@ -0,0 +1,9 @@
+"""
+В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
+"""
+from sys import argv
+
+from lesson_06.task_7.task_07 import dat
+from sys import argv
+
+print(dat(argv[1]))
